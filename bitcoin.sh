2332da095149ee87e139678db74a2a42c0e2ef30
Matt Parker <parker20121@users.noreply.github.com>
2016-07-13 22:05:32 -0400

    Update bitcoin.sh

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,7 +1,7 @@
 # ensure bitcoin_filter.py has the proper permissions
 
 hadoop jar [path to hadoop_streaming.jar] \
-    -jobconf mapred.reduce.tasks=0 \
+        -jobconf mapred.reduce.tasks=0 \
 	-mapper "python bitcoin_filter.py" \
 	-input /tmp/bitcoin/data \
 	-output /tmp/bitcoin/output \
